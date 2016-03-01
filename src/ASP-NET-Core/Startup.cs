e8ce3b6f65deb434f7d8742c747d85f9c83d85b2
Maledetti Informatici <info@marcellosalamone.it>
2016-03-01 13:09:43 +0100

    ASP.NET Core, Le Configurazioni
    Integrazione delle implementazioni sulle configurazioni. Post del
    01/03/2016 MaledettiInformatici url:  http://www.marcellosalamone.it

--- src/ASP-NET-Core/Startup.cs
+++ src/ASP-NET-Core/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNet.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNet.Diagnostics;
 
+using Microsoft.Extensions.Configuration;               // libreria per l'accesso alle configurazioni 
+
 namespace ASP_NET_Core
 {
     public class Startup
@@ -23,29 +25,29 @@ namespace ASP_NET_Core
         {
             app.UseIISPlatformHandler();
 
-            // aggiungo il middleware per la gestione della pagina di errore dopo avere impostato su 
-            // project.json il riferimento a Microsoft.AspNet.Diagnostics
+            // aggiungo il middleware per la gestione dell'errore (project.json --> Microsoft.AspNet.Diagnostics)
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseRuntimeInfoPage(); // il path di default è  /runtimeinfo
+                app.UseRuntimeInfoPage();           // il path di default è  /runtimeinfo
             }
             else
             {
-                // specifico il comportamento che l'applicazione deve eseguire in caso di errore 
-                // se la riga seguente viene commentata, l'exception non verrà catturata 
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Home/Error");  // risorsa da cercare in caso di exception 
             }
-            app.UseWelcomePage("/welcome");
+            app.UseWelcomePage("/welcome");         // risorsa per il welcome 
 
+            // carico le configurazioni definite su project.json 
+            LoadSettings();
 
-            // genero il response per la creazione di un indice che permetta di linkare o la pagina 
-            // di benvenuto alla risorsa /welcome  oppure lancio l'exception per simulare la routine
+            // genero response per invocare la welcome o l'errore 
             app.Run(async (context) =>
             {
                 if (context.Request.Query.ContainsKey("throw")) throw new Exception("Exception triggered!");
                 context.Response.ContentType = "text/html";
                 await context.Response.WriteAsync("<html><body>Hello World!");
+                await context.Response.WriteAsync(string.Format("<p>impostazioni: </br> -->key1 = {0}</br> -->key2 = {1}</p>", MySettings[0], MySettings[1]));
+                await context.Response.WriteAsync(string.Format("<p>impostazioni: </br> -->key3 = {0}</br> -->key4 = {1}</p>", MySettings[2], MySettings[3]));
                 await context.Response.WriteAsync("<ul>");
                 await context.Response.WriteAsync("<li><a href=\"/welcome\">Welcome Page</a></li>");
                 await context.Response.WriteAsync("<li><a href=\"/?throw=true\">Throw Exception</a></li>");
@@ -53,6 +55,34 @@ namespace ASP_NET_Core
                 await context.Response.WriteAsync("</body></html>");
             });
         }
+
+        public static string[] MySettings = new string[4];
+
+
+        /// <summary>
+        /// Load Settings from resource json files
+        /// </summary>
+        public static void LoadSettings()
+        {
+            // impostazione di una configurazione 
+            // var builder = new ConfigurationBuilder().AddJsonFile("project.json");
+
+            // impostazione di una configurazione su più risorse 
+            var builder = new ConfigurationBuilder()
+                    .AddJsonFile("project.json")
+                    .AddJsonFile("appsettings.json", optional: true);
+
+            var config = builder.Build();
+
+            // estrazione della configurazione command:web
+            //string commands = config["commands:web"];
+
+            MySettings[0] = config["mysettings:key1"];
+            MySettings[1] = config["mysettings:key2"];
+            MySettings[2] = config["mysettings:key3"];
+            MySettings[3] = config["mysettings:key4"];
+        }
+
 
         // Entry point for the application.
         public static void Main(string[] args) => WebApplication.Run<Startup>(args);
